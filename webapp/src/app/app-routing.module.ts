27be89864847205f4aae089cd0ca21cad00a04c8
Hubert Mikołajewicz <hubmikvvv@gmail.com>
2021-01-08 00:55:25 +0100

    Add server app with app context, physical db, and basic routing in frontend application

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { HomeComponent } from './components/home/home.component';
+import { ExploitationComponent } from './components/exploitation/exploitation.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'user-profile', component: UserProfileComponent },
+  { path: 'exploitation', component: ExploitationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ExploitationComponent } from './components/exploitation/exploitation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    NavComponent,
+    ExploitationComponent
   ],
   imports: [
     BrowserModule,
