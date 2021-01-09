cb33bb807186a06aa2c32f7382b11580f66e602b
Hubert Mikołajewicz <hubmikvvv@gmail.com>
2021-01-09 03:33:46 +0100

    Working method for register users in API - tested in postman

---
+++ WebAPI/WebAPI/Controllers/ApplicationUserController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Infrastructure;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUserController : ControllerBase
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+
+        public ApplicationUserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [Route("RegisterUser")]
+        //POST : /api/ApplicationUser/Register
+        public async Task<object> PostApplicationUser(Models.ApplicationUser model)
+        {
+            var applicationUser = new ApplicationUser
+            {
+                UserName = model.UserName,
+                FullName = model.FullName,
+                Email = model.Email
+            };
+
+            var result = await _userManager.CreateAsync(applicationUser, model.Password);
+
+            return Ok(result);
+        }
+    }
+}
