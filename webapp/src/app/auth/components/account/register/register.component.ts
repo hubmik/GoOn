4507101363d338c1820a41c0b7c952c1e3a809b4
Hubert Mikołajewicz <hubmikvvv@gmail.com>
2021-02-22 19:36:09 +0100

    Implement logic for registration in register component

--- webapp/src/app/auth/components/account/register/register.component.ts
+++ webapp/src/app/auth/components/account/register/register.component.ts
@@ -11,9 +11,27 @@ export class RegisterComponent implements OnInit {
   constructor(public service: RegistrationService) { }
 
   ngOnInit() {
+    this.service.formModel.reset();
   }
 
   onSubmitForm(){
+    this.service.register().subscribe(
+      (res: any) => {
+        if(res.succeeded){
+          this.service.formModel.reset();
+        }else{
+          res.errors.array.forEach(element => {
+            switch(element.code){
+              case 'DuplicateUserName':
+                break;
 
+              default:
+                break;
+            }
+          });
+        }
+      },
+      err => { console.log(err) }
+    );
   }
 }
