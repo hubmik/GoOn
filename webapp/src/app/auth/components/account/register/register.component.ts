b9bf7992fee68645a347ecf8da0e793eaae86f41
Hubert Mikołajewicz <hubmikvvv@gmail.com>
2021-02-23 13:27:52 +0100

    Push feature branch into remote

--- webapp/src/app/auth/components/account/register/register.component.ts
+++ webapp/src/app/auth/components/account/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { RegistrationService } from '../../../services/registration.service';
 
